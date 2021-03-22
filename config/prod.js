715a02d05706c9669aaeb375e2bb444a4059549a
Lotan <lotan.mash@gmail.com>
2021-03-22 19:20:43 +0200

    lotan in da house!

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,3 @@
 module.exports = {
   'dbURL': 'mongodb+srv://master:6f4S2FmzLFsJdjcm@cluster0.nxnde.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-<<<<<<< HEAD
-=======
-
->>>>>>> 3c5ca236fc6ac56825d3e58adb0faaf2adf43edb
 }
